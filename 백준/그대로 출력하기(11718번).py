25efc944d6c09898cae8729e140587251e80af81
janghs <seokchu123@gmail.com>
2025-03-18 19:44:31 +0900

    add: 11718, 11720

---
+++ 백준/그대로 출력하기(11718번).py
@@ -0,0 +1,7 @@
+import sys
+
+s = sys.stdin.read()
+results = s.splitlines()
+
+for result in results:
+    print(result)
---
+++ 백준/숫자의 합(11720번).py
@@ -0,0 +1,7 @@
+N = int(input())
+a = input()
+sum = 0
+
+for i in range(len(a)):
+    sum += int(a[i])
+print(sum)
